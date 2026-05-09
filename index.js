22ad6e5e5a467acb7ca993ebb8f786afd36dbd42
= <=>
2026-05-09 13:12:01 -0400

    initial commit, no interactivity

---
+++ index.js
@@ -0,0 +1,19 @@
+function OnRemove(){
+    console.log(this.target)
+}
+
+function OnDuplicate(e){
+
+}
+
+function OnMoveDown(e){
+
+}
+
+function OnMoveHeld(e){
+
+}
+
+function OnMoveRelease(e){
+
+}
